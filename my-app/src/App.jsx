89d9831167f57618ed4736f7478538928aa6b0d0
Abhishek <abhishek29730@gmail.com>
2026-04-09 22:49:04 +0530

    Initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect} from "react";
+import gsap from "gsap";
+import {ScrollTrigger, SplitText, ScrollSmoother} from "gsap/all"; 
+
+import Warp from './components/Warp/Warp'
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+
+const App = () => {
+ useEffect(() => {
+  ScrollSmoother.create({
+    smooth: 3,
+    effects: true,
+    normalizeScroll: true,
+  });
+
+  ScrollTrigger.refresh();
+ }, []);
+
+  return(
+    <>
+      <Warp/>
+    </>
+  )
+}
+
+export default App
+
